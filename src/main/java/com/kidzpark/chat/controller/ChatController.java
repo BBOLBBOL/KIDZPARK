fcce47e3ae7a19407f769502aed93cd426a586a2
GGG <GGG@DESKTOP-8N3GG4O>
2023-11-17 10:44:14 +0900

    2023-11-17 10:44

--- src/main/java/com/kidzpark/chat/controller/ChatController.java
+++ src/main/java/com/kidzpark/chat/controller/ChatController.java
@@ -1,29 +1,19 @@
 package com.kidzpark.chat.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/Chat")
+@Controller
 public class ChatController {
-
-    private final ChatService chatService;
-
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    private final SimpMessageSendingOperations messagingTemplate;
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
